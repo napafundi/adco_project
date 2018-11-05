a63edb717dfa9beffa7c6c509c199d058c426342
Nick Pafundi <napafundi@gmail.com>
2018-11-05 13:26:52 -0500

    second commit, worked on making second table show through tab click, not finished

--- backend.py
+++ backend.py
@@ -1,15 +1,15 @@
 import sqlite3
 
-class Database:
+class Balance_Database:
 
-    def __init__(self):
-        self.conn = sqlite3.Connection("balance.db")
+    def __init__(self, database):
+        self.conn = sqlite3.Connection(database)
         self.cur = self.conn.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS balance (date TEXT, amount REAL)")
         self.conn.commit()
 
 
-    def change_balance(self,date, amount):
+    def change_balance(self,date,amount):
         self.cur.execute("INSERT INTO balance VALUES (?,?)", (date,amount))
         self.conn.commit()
 
@@ -17,6 +17,28 @@ class Database:
         global rows
         self.cur.execute("SELECT * FROM balance")
         self.rows = self.cur.fetchall()
+
+    def __del__(self):
+        self.conn.close()
+
+class Transaction_Database:
+
+    def __init__(self, database):
+        self.conn = sqlite3.Connection(database)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS transactions (date TEXT, amount REAL, location TEXT, type TEXT)")
+        self.conn.commit()
+
+    def change_balance(self,date,amount,location,type):
+        self.cur.execute("INSERT INTO transactions VALUES (?,?,?,?)", (date,amount,location,type))
+        self.conn.commit()
+
+    def view(self):
+        global rows
+        self.cur.execute("SELECT * FROM transactions")
+        self.rows = self.cur.fetchall()
+        print(self.rows)
+
 
     def __del__(self):
         self.conn.close()
--- bank.py
+++ bank.py
@@ -5,30 +5,63 @@ from tkinter import ttk
 
 window = Tk()
 window.title("Bank Account")
-database = backend.Database()
+balance_db = backend.Balance_Database("balance.db")
+transaction_db = backend.Transaction_Database("transactions.db")
 
 #button commands
 def balance():
-    database.view()
-    for row in database.rows:
+    balance_db.view()
+    for row in balance_db.rows:
         tree.insert("",END,values = row)
+
+def transactions():
+    transaction_db.view()
+    for row in transaction_db.rows:
+        tree_2.insert("",END,values = row)
+
+
+def click(event):
+    print("hello")
+    transactions()
+
 
 #create tabs
 notebook = ttk.Notebook(window)
 f1 = ttk.Frame(notebook)
 f2 = ttk.Frame(notebook)
+f2.bind("<ButtonRelease-1>", click)
 notebook.add(f1, text = "Balance")
 notebook.add(f2, text = "Transactions")
 notebook.pack(expand=1, fill="both")
 
+
+
 #create balance table
 tree = ttk.Treeview(window, column = ("Date","Amount"), show = "headings")
+tree.column("Date", anchor="center")
+tree.column("Amount", anchor="center")
 tree.heading("#1", text = "Date")
 tree.heading("#2", text = "Amount")
-tree.pack()
+tree.pack(fill = "both", expand = True)
+
+#create a transactions table
+tree_2 = ttk.Treeview(window, column = ("Date","Amount","Location","Type"), show = "headings")
+tree_2.column("Date", anchor="center")
+tree_2.column("Amount", anchor="center")
+tree_2.column("Location", anchor="center")
+tree_2.column("Type", anchor="center")
+tree_2.heading("#1", text = "Date")
+tree_2.heading("#2", text = "Amount")
+tree_2.heading("#3", text = "Location")
+tree_2.heading("#4", text = "Type")
+tree_2.pack(fill = "both", expand = True)
+
 
 
 #display the application
-#database.change_balance(r"11/20/2018", 30.00)
+#transaction_db.change_balance("11/5/2018",35.00, "Target", "Food")
+transaction_db.view()
+
 balance()
+transactions()
 window.mainloop()
