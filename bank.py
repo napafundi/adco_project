6b6d3979b61d3f4f639d78eed0b7b1a69065fe33
Nick Pafundi <napafundi@gmail.com>
2018-11-08 11:48:13 -0500

    edited tabs to display when clicked upon

--- bank.py
+++ bank.py
@@ -20,16 +20,11 @@ def transactions():
         tree_2.insert("",END,values = row)
 
 
-def click(event):
-    print("hello")
-    transactions()
-
 
 #create tabs
 notebook = ttk.Notebook(window)
 f1 = ttk.Frame(notebook)
 f2 = ttk.Frame(notebook)
-f2.bind("<ButtonRelease-1>", click)
 notebook.add(f1, text = "Balance")
 notebook.add(f2, text = "Transactions")
 notebook.pack(expand=1, fill="both")
@@ -37,30 +32,30 @@ notebook.pack(expand=1, fill="both")
 
 
 #create balance table
-tree = ttk.Treeview(window, column = ("Date","Amount"), show = "headings")
+tree = ttk.Treeview(f1, column = ("Date","Amount"), show = "headings")
 tree.column("Date", anchor="center")
 tree.column("Amount", anchor="center")
-tree.heading("#1", text = "Date")
-tree.heading("#2", text = "Amount")
+tree.heading("#1", text="Date")
+tree.heading("#2", text="Amount")
 tree.pack(fill = "both", expand = True)
 
 #create a transactions table
-tree_2 = ttk.Treeview(window, column = ("Date","Amount","Location","Type"), show = "headings")
+tree_2 = ttk.Treeview(f2, column = ("Date","Amount","Location","Type"), show = "headings")
 tree_2.column("Date", anchor="center")
-tree_2.column("Amount", anchor="center")
+tree_2.column("Amount", anchor="center" )
 tree_2.column("Location", anchor="center")
 tree_2.column("Type", anchor="center")
-tree_2.heading("#1", text = "Date")
-tree_2.heading("#2", text = "Amount")
-tree_2.heading("#3", text = "Location")
-tree_2.heading("#4", text = "Type")
+tree_2.heading("#1", text="Date")
+tree_2.heading("#2", text="Amount")
+tree_2.heading("#3", text="Location")
+tree_2.heading("#4", text="Type")
 tree_2.pack(fill = "both", expand = True)
 
 
 
 #display the application
 #transaction_db.change_balance("11/5/2018",35.00, "Target", "Food")
-transaction_db.view()
+#transaction_db.view()
 
 balance()
 transactions()
